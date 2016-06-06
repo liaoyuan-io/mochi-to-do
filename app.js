bdab8c9b98d2ea4ad0d2b4d4c75781d2989c6155
Wei Guangcheng <twksos@qq.com>
2016-06-05 21:05:17 -0400

    implement add new item to todo list

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 (function () {
     'use strict';
+    const ENTER_KEY = 13;
     angular.module('mochiToDo', []).controller('TodoCtrl', function TodoCtrl($scope) {
         $scope.items = [
             {content: '当有待办事项存在时，我希望能看到待办事项列表', isDone: false},
@@ -7,6 +8,14 @@
             {content: '当我点击待办事项左侧的checkbox时，待办事项应该标示为已完成', isDone: false},
             {content: '当我点击已完成待办事项左侧的checkbox时，待办事项应该标示为未完成', isDone: false},
             {content: '当我点击待办事项右侧的关闭按钮时，待办事项应当被删除', isDone: false}
-        ]
+        ];
+        $scope.newItem = '';
+        $scope.submitItem = function (keyCode) {
+            if(keyCode == ENTER_KEY) {
+                const addedItem = $scope.newItem;
+                $scope.newItem = '';
+                $scope.items.push({content:addedItem, isDone:false})
+            }
+        }
     })
 })();
