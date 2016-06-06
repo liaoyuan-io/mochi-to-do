41f470d7016e8b51182244d42e6267c0d2062c9e
Wei Guangcheng <twksos@qq.com>
2016-06-05 20:48:41 -0400

    add dummy data for todo list

---
+++ app.js
@@ -0,0 +1,12 @@
+(function () {
+    'use strict';
+    angular.module('mochiToDo', []).controller('TodoCtrl', function TodoCtrl($scope) {
+        $scope.items = [
+            {content: '当有待办事项存在时，我希望能看到待办事项列表', isDone: false},
+            {content: '当我输入文字并使用回车确认时，我希望新输入的待办事项项目显示在列表中', isDone: false},
+            {content: '当我点击待办事项左侧的checkbox时，待办事项应该标示为已完成', isDone: false},
+            {content: '当我点击已完成待办事项左侧的checkbox时，待办事项应该标示为未完成', isDone: false},
+            {content: '当我点击待办事项右侧的关闭按钮时，待办事项应当被删除', isDone: false}
+        ]
+    })
+})();
