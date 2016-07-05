7aaf3cc0b7465149b6c27b50b4106cf7065d290f
Wei Guangcheng <twksos@qq.com>
2016-07-05 00:51:14 -0400

    add input

--- app/MochiToDo/index.ios.js
+++ app/MochiToDo/index.ios.js
@@ -8,6 +8,7 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
+  TextInput,
   Text,
   View
 } from 'react-native';
@@ -16,6 +17,11 @@ class MochiToDo extends Component {
   render() {
     return (
       <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} 
+        placeholder="请输入待办事项..."
+        />
+      </View>
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
@@ -34,9 +40,24 @@ class MochiToDo extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+  },
+  inputContainer: {
+    alignSelf:'stretch', 
+    backgroundColor: 'white', 
+    borderRadius: 4,
+    paddingVertical: 8,
+    paddingHorizontal: 24,
+    marginTop: 20 + 24,
+    marginHorizontal: 24,
+    marginBottom: 24,
+  },
+  input: {
+    height: 24, 
+    fontSize: 20, 
+    lineHeight: 24
   },
   welcome: {
     fontSize: 20,
