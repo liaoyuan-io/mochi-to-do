2558e43402d266c36f9ddcdd3afbf6c1380d1a3e
Wei Guangcheng <twksos@qq.com>
2016-07-05 01:20:12 -0400

    extract component for to do item

--- app/MochiToDo/index.ios.js
+++ app/MochiToDo/index.ios.js
@@ -9,9 +9,32 @@ import {
   AppRegistry,
   StyleSheet,
   TextInput,
+  ScrollView,
   Text,
   View
 } from 'react-native';
+
+class ToDoItem extends Component {
+  renderCheckbox(checked){
+    if(checked) return <View style={[styles.iconContainer, {backgroundColor: 'grey', borderWidth: 1, borderColor: 'black'}]}>
+      <Text style={styles.icon}>✔️</Text>
+    </View> 
+    return <View style={[styles.iconContainer, {backgroundColor: 'white', borderWidth: 1, borderColor: 'black'}]}>
+    </View> 
+  }
+  render() {
+    const checked = this.props.checked;
+    return <View style={styles.toDoItemContainer}>
+      {this.renderCheckbox(checked)}
+      <View style={styles.todoItemContent}>
+        <Text style={[styles.todoItemContentText, checked && {textDecorationLine: 'line-through'}]}>{this.props.children}</Text>
+      </View>
+      <View style={styles.iconContainer}>
+        <Text style={styles.icon}>✖️</Text>
+      </View>
+    </View>
+  }
+}
 
 class MochiToDo extends Component {
   render() {
@@ -22,17 +45,14 @@ class MochiToDo extends Component {
         placeholder="请输入待办事项..."
         />
       </View>
-      <View style={styles.toDoItemContainer}>
-        <View style={[styles.iconContainer, {backgroundColor: 'white', borderWidth: 1, borderColor: 'black'}]}>
-          <Text style={styles.icon}>✔️</Text>
-        </View>
-        <View style={styles.todoItemContent}>
-          <Text style={styles.todoItemContentText}>待办事项一</Text>
-        </View>
-        <View style={styles.iconContainer}>
-          <Text style={styles.icon}>✖️</Text>
-        </View>
-      </View>
+      <ScrollView>
+        <ToDoItem checked>实现基本样式</ToDoItem>
+        <ToDoItem>当有待办事项存在时，我希望能看到待办事项列表。</ToDoItem>
+        <ToDoItem>当我输入文字并使用回车确认时，我希望新输入的待办事项项目显示在列表中。</ToDoItem>
+        <ToDoItem>当我点击待办事项左侧的 Checkbox 时，待办事项应该标示为已完成。</ToDoItem>
+        <ToDoItem>当我点击已完成待办事项左侧的 Checkbox 时，待办事项应该标示为未完成。</ToDoItem>
+        <ToDoItem>当我点击待办事项右侧的「×」按钮时，待办事项应当被删除。</ToDoItem>
+      </ScrollView>
       </View>
     );
   }
@@ -61,7 +81,7 @@ const styles = StyleSheet.create({
   },
   toDoItemContainer: {
     flexDirection:'row', 
-    alignItems: 'center', 
+    alignItems: 'center',
     padding: 24,
     borderTopWidth: 1,
     borderTopColor: 'black'
