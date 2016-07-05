cd41b8f4d06553bd5807a972d2b2ea7d5e85afca
Wei Guangcheng <twksos@qq.com>
2016-07-05 01:35:17 -0400

    add model

--- app/MochiToDo/index.ios.js
+++ app/MochiToDo/index.ios.js
@@ -14,6 +14,39 @@ import {
   View
 } from 'react-native';
 
+const TODOS = [
+  {id: 0, content: '当有待办事项存在时，我希望能看到待办事项列表', isDone: false},
+  {id: 1, content: '当我输入文字并使用回车确认时，我希望新输入的待办事项项目显示在列表中', isDone: false},
+  {id: 2, content: '当我点击待办事项左侧的checkbox时，待办事项应该标示为已完成', isDone: false},
+  {id: 3, content: '当我点击已完成待办事项左侧的checkbox时，待办事项应该标示为未完成', isDone: false},
+  {id: 4, content: '当我点击待办事项右侧的关闭按钮时，待办事项应当被删除', isDone: false}
+];
+
+class ToDo {
+  constructor(items = []){
+    this.items = items;
+  }
+  add(content){
+    const id = this.items.length
+    this.items.push({
+      id: id, content: content, isDone: false
+    })
+  }
+  check(id){
+    this.items = this.items.map((item)=>{
+      if(item.id !== id) return item;
+      return Object.assign({},item, {isDone: !item.isDone});
+    })
+  }
+  remove(id){
+    this.items = this.items.filter((item)=>{
+      return item.id !== id
+    })
+  }
+}
+
+const toDoData = new ToDo(TODOS);
+
 class ToDoItem extends Component {
   renderCheckbox(checked){
     if(checked) return <View style={[styles.iconContainer, {backgroundColor: 'grey', borderWidth: 1, borderColor: 'black'}]}>
@@ -40,16 +73,8 @@ class MochiToDo extends Component {
   constructor(){
     super();
     this.state = {
-      todos: []
+      todos: toDoData.items
     }
-  }
-  componentWillMount() {
-    const TODOS = [{id: 0, content: '当有待办事项存在时，我希望能看到待办事项列表', isDone: false},
-        {id: 1, content: '当我输入文字并使用回车确认时，我希望新输入的待办事项项目显示在列表中', isDone: false},
-        {id: 2, content: '当我点击待办事项左侧的checkbox时，待办事项应该标示为已完成', isDone: false},
-        {id: 3, content: '当我点击已完成待办事项左侧的checkbox时，待办事项应该标示为未完成', isDone: false},
-        {id: 4, content: '当我点击待办事项右侧的关闭按钮时，待办事项应当被删除', isDone: false}];
-    this.setState({todos: TODOS});
   }
   render() {
     return (
